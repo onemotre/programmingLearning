183b2056401bc44a599e194f53839ecbecb09dc1
onemortre <130.toolwolrd@gmail.com>
2021-02-02 22:56:10 +0800

    init

---
+++ java/testInstall/hello.java
@@ -0,0 +1,13 @@
+public class hello
+{
+	public static void main(String[] args)
+	{
+		String greeting = "Hello World!";
+		System.out.println(greeting);
+		for (int i = 0; i < greeting.length(); i++)
+		{
+			System.out.print("=");
+		}
+		System.out.println();
+	}
+}
