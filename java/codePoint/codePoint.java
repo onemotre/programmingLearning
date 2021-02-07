927143b90dad0931d09a4330e1eb8188a690fffb
onemortre <130.toolwolrd@gmail.com>
2021-02-07 23:09:00 +0800

    update codePoint.java

--- java/codePoint/codePoint.java
+++ java/codePoint/codePoint.java
@@ -16,19 +16,35 @@ public class codePoint{
         for(int i = 0; i <= greeting.length() - 1; i++)
             System.out.print('=');
         System.out.println();
+        
+        while(true){
+            System.out.println("'1' is for code point to string");
+            System.out.println("'2' is for string to code point");
+            System.out.println("'3' is to exit");
 
-        System.out.println("'1' is for code point to string");
-        System.out.println("'2' is for string to code point");
-
-        int model = in.nextInt();
-        if (model == 2){
-            String sentence = in.next();
-            for(int i = 0; i<=sentence.length()-1; ){
-                int cp = sentence.codePointAt(i);
-                if(Character.isSupplementaryCodePoint(cp))  i+=2;
-                else i++;
-                System.out.print(cp);
+            int model = in.nextInt();
+            if (model == 1){
+                String str = in.next();
+                int[] codePoints = new int[str.codePointCount(0, str.length())];
+                for (int i = 0; i<=str.length() - 1; i=i+3){
+                    codePoints[i] = Integer.parseInt(str.substring(i, i+3));
+                }   
+                String str2 = new String(codePoints,0,codePoints.length);
+                System.out.println(str2);
             }
-        }
+
+            if (model == 2){
+                String sentence = in.next();
+                for(int i = 0; i<=sentence.length()-1; ){
+                    int cp = sentence.codePointAt(i);
+                    if(Character.isSupplementaryCodePoint(cp))  i+=2;
+                    else i++;
+                    System.out.println(cp);
+                }
+            }
+            else{
+                break;
+            }
+        }       
    }
 }
