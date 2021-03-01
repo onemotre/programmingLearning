de273ef46450271f67c66b9a5523620211163155
onemortre <130.toolwolrd@gmail.com>
2021-03-01 12:47:42 +0800

    init

---
+++ java/equalAnalysis/equalAnalysis.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+/**
+ * Analysis the equalments
+ * @author onemotre
+ * @version 02.08.2021
+ */
+
+public class equalAnalysis {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("type your equalment below:");
+        String formula = in.nextLine();
+
+        char[] operator = new char[10];
+        int operatorNum = 0;
+        int[] array = new int[10];
+        for(int i = 0;i <= 10; i++){
+            array[i] = 0;
+        }
+
+        String number = "";
+        for(int i= 0;i <= formula.length();i++){
+            if(formula.charAt(i) >= '9' || formula.charAt(i) <= '0'){
+                array[operatorNum] = Integer.parseInt(number);
+                number = "";
+                operator[operatorNum] = formula.charAt(i);
+                operatorNum++;
+            }
+            else
+                number = number + String.valueOf(formula.charAt(i));
+        }
+        for(int element : array)
+            System.out.print(element + " ");
+        for(char element2 : operator)
+            System.out.print(element2 + " ");
+    }
+}
